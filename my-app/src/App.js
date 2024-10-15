bb55a438d7bb89c7eeb60487b47fb65ac980cad6
Andrew Thomas <athomas94@gmail.com>
2024-10-14 21:29:49 -0400

    added banner and respective styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './Components/Navigation';
+import Banner from './Components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Banner />
     </div>
   );
 }
